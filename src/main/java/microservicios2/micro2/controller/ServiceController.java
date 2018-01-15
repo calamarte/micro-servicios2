e3822a93733df63e2e143f26bf40f3473a0e089b
calamarte <calamarte@gmail.com>
2018-01-15 21:15:05 +0100

    haciendo cosas que creo que tienen sentido

--- src/main/java/microservicios2/micro2/controller/ServiceController.java
+++ src/main/java/microservicios2/micro2/controller/ServiceController.java
@@ -1,10 +1,15 @@
 package microservicios2.micro2.controller;
 
+import microservicios2.micro2.discover.Discover;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ServiceController {
+
+    @Autowired
+    Discover discover;
 
     @RequestMapping("/controller")
     public String prueba(){
