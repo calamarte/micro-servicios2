c4b57c3dd91c1dbaaba779baa9df0978b95a9a79
Javi Rotger <fjrotgerl@gmail.com>
2018-01-17 17:32:29 +0100

    Hilo pasado al controller

--- src/main/java/microservicios2/micro2/controller/ServiceController.java
+++ src/main/java/microservicios2/micro2/controller/ServiceController.java
@@ -1,7 +1,10 @@
 package microservicios2.micro2.controller;
 
 import microservicios2.micro2.discover.Discover;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,5 +28,12 @@ public class ServiceController {
         peer.setName(name);
 
         discover.insert(peer);
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(ServiceController.class);
+
+    @Scheduled(fixedRate = 500)
+    public void reportCurrentTime() {
+        log.info("The time is now {}");
     }
 }
