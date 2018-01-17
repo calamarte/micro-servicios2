2a5bc8f783e0fccdda5b8ddae87714482a95329a
Javi Rotger <fjrotgerl@gmail.com>
2018-01-17 17:16:12 +0100

    Hilo montado, pero no hace lo que queremos todavia

--- src/main/java/microservicios2/micro2/Micro2Application.java
+++ src/main/java/microservicios2/micro2/Micro2Application.java
@@ -1,11 +1,18 @@
 package microservicios2.micro2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
+@EnableAsync
 public class Micro2Application {
 
 	public static void main(String[] args) {
