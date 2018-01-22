7d569391d7f84bd2cfc9208cf39c93b00c350b81
Rubén Mariscal <mariscal.rmc@gmail.com>
2018-01-22 21:09:28 +0100

    properties

--- src/main/java/microservicios2/micro2/controller/ServiceController.java
+++ src/main/java/microservicios2/micro2/controller/ServiceController.java
@@ -1,7 +1,9 @@
 package microservicios2.micro2.controller;
 
 import microservicios2.micro2.discover.Discover;
+import org.omg.CORBA.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +11,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
+
 @RestController
 public class ServiceController {
 
     @Autowired
     private Discover discover;
 
-    @RequestMapping(value = "/controller/{ip}/{time}/{name}",method = RequestMethod.POST)
+    @RequestMapping(value = "/${controller}/{ip}/{time}/{name}",method = RequestMethod.POST)
     public void getMessage(@PathVariable("ip") String ip,
                              @PathVariable("time") String time,
                              @PathVariable("name") String name){
@@ -31,13 +34,8 @@ public class ServiceController {
         discover.insert(peer);
     }
 
-    @RequestMapping(path = "/peers")
-    public String showPeers(){
-        return discover.getDiscoverCache().toString();
-    }
 
-
-    @Scheduled(fixedRate = 3000)
+    @Scheduled(fixedRateString = "${discoverInterval}")
     public void reportCurrentTime() throws Exception {
         discover.sendBroadcast();
     }
--- src/main/java/microservicios2/micro2/discover/DiscoverCache.java
+++ src/main/java/microservicios2/micro2/discover/DiscoverCache.java
@@ -1,68 +1,67 @@
 package microservicios2.micro2.discover;
 
-import com.sun.javafx.util.Logging;
 import microservicios2.micro2.controller.Peer;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Calendar;
-import org.slf4j.Logger;
 
 
 @Component
 public class DiscoverCache implements Cache{
+
+    @Value("${inactivityLimit}")
+    int inactivityLimit;
+
     private ArrayList<Peer> discoveredPeers = new ArrayList<>();
-    private Logger logger = LoggerFactory.getLogger(DiscoverCache.class);
 
     public void insertPeer(Peer newPeer){
         System.out.println("--------------------------------------------------------");
         for (Peer peer : discoveredPeers) {
-            logger.info("Peer found");
-            logger.info("Info peer: " + peer.toString());
+            System.out.println(peer.toString());
         }
         System.out.println("--------------------------------------------------------");
         if(exist(newPeer.getIp())){
             update(newPeer);
             return;
         }
+
         discoveredPeers.add(newPeer);
     }
 
-    @Scheduled(fixedRate =  30 * 60 * 1000)
+    @Scheduled(fixedRateString =  "${purgueInterval}")
     public void purge(){
+
         long currentTime = Calendar.getInstance().getTime().getTime();
         for(int peerIndex = 0; peerIndex < discoveredPeers.size(); peerIndex++){
             Peer peer = discoveredPeers.get(peerIndex);
-            if(peer.getDate().getTime() < (currentTime - 15 * 60 * 1000)){
+            if(peer.getDate().getTime() < (currentTime - inactivityLimit)){
+                System.out.println("entra");
                 discoveredPeers.remove(peerIndex);
             }
+
+            System.out.println(peer.getDate().getTime());
         }
-        logger.info("Peer purged");
+        System.out.println("purgado");
     }
 
-
     private boolean exist(String ip){
-        DiscoverExistsLambda existsLambda = string -> discoveredPeers.stream().anyMatch(peer -> peer.getIp().equals(string));
-        return existsLambda.exists(ip);
+        for (Peer peer :discoveredPeers) {
+            if(peer.getIp().equals(ip)){
+                return true;
+            }
+        }
+        return false;
     }
 
     private void update(Peer peerToUpdate){
-        DiscoverUpdateLambda updateLambda = peerNoUpdated -> discoveredPeers.stream().filter(peer -> peer.getIp().equals(peerNoUpdated.getIp())).forEach(peer -> {
-            peer.setDate(peerNoUpdated.getDate());
-            peer.setName(peerNoUpdated.getName());
-        });
-        updateLambda.update(peerToUpdate);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder peers = new StringBuilder();
         for (Peer peer :discoveredPeers) {
-            peers.append(peer.toString());
-            peers.append("</br>");
+            if (peer.getIp().equals(peerToUpdate.getIp())){
+                peer.setDate(peerToUpdate.getDate());
+                peer.setName(peerToUpdate.getName());
+            }
         }
-        return peers.toString();
     }
 
 }
